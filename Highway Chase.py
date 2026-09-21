40eeb242d577b04ac6b8d9db461346169fb1ce48
ronakmelwani25 <ronakmelwani25@gmail.com>
2026-09-21 19:36:43 +0100

    Add files via upload

--- Highway Chase.py
+++ Highway Chase.py
@@ -25,10 +25,26 @@ Corvette=pygame.image.load("Python/Pygame/Lesson 4 - Space Shooter/Images/overhe
 cscale=pygame.transform.scale(Corvette,(160,260))
 crotate=pygame.transform.rotate(cscale,360)
 
+Bugatti=pygame.image.load("Python/Pygame/Lesson 4 - Space Shooter/Images/Over-removebg-preview.png")
+bscale=pygame.transform.scale(Bugatti,(280,190))
+brotate=pygame.transform.rotate(bscale,90)
+
+F1=pygame.image.load("Python\Pygame\Lesson 4 - Space Shooter\Images\Lotus_F1-removebg-preview (1).png")
+fscale=pygame.transform.scale(F1,(300,120))
+frotate=pygame.transform.rotate(fscale,90)
+
 mrect=pygame.Rect(270,160,270,160)
 trect=pygame.Rect(100,100,330,250)
 crect=pygame.Rect(1300,-100,260,160)
 krect=pygame.Rect(1000,1100,300,200)
+brect=pygame.Rect(300,300,280,190)
+frect=pygame.Rect(750,500,300,120)
+
+p=1
+pp=2
+ppp=3
+pppp=4
+ppppp=5
 
 def mover():
     if keys_pressed[pygame.K_UP]:
@@ -50,20 +66,34 @@ while True:
     screen.blit(trotate, (trect))
     screen.blit(crotate, (crect))
     screen.blit(krotate, (krect))
+    screen.blit(brotate, (brect))
+    screen.blit(frotate, (frect))
     keys_pressed = pygame.key.get_pressed()
-    p=random.randint(1,7)
-    pp=random.randint(1,5)
+    print(pppp)
     mover()
 
     krect.y=krect.y-p
     if krect.y<=-300:
         krect.y=1000
         krect.x=random.randint(800,1300)
+        p=random.randint(1,7)
 
     crect.y=crect.y+pp
-    print(crect.y)
     if crect.y>=1000:
         crect.y=-100
         crect.x=random.randint(800,1300)
+        pp=random.randint(1,5)
+    
+    brect.y=brect.y+ppp
+    if brect.y>=1000:
+        brect.y=-100
+        brect.x=random.randint(150,700)
+        ppp=random.randint(2,6)
+    
+    trect.y=trect.y-pppp
+    if trect.y<=-300:
+        trect.y=1000
+        trect.x=random.randint(150,700)
+        pppp=random.randint(2,4)
 
     pygame.display.update()
